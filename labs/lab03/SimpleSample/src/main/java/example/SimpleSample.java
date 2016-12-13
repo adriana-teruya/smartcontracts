5af746e9abd8c498b0b3c693bfe640ca7816bbb4
adriana-teruya <adriana_teruya@hotmail.com>
2016-12-13 14:45:17 -0200

    Update SimpleSample.java

--- labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
+++ labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
@@ -150,7 +150,7 @@ public class SimpleSample extends ChaincodeBase {
 
 	@Override
 	public String getChaincodeID() {
-		return "SimpleSample";
+		return "ateruya";
 	}
 
 	public static void main(String[] args) throws Exception {
